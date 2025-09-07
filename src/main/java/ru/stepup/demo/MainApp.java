2938e232dc598bc9fe4552ab28b40fc06942930a
Егор <xefver@gmail.com>
2025-09-07 18:13:18 +0300

    Доработка вывода 4 цифр

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -3,12 +3,13 @@ package ru.stepup.demo;
 public class MainApp{
 
     public static void main(String[] args) {
-        int a;
-        int b;
+        int a=5;
+        int b=6;
         System.out.println(1);
         // TODO поменять имя на enum
         System.out.println(2);
-        System.out.println(3);
+        System.out.println(a);
+        System.out.println(b);
 
     }
 }
