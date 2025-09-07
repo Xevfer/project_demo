0e46452e393b084755799644e58170134fcf147a
Егор <xefver@gmail.com>
2025-09-07 18:32:18 +0300

    Такса 002 , улучшил логику main

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -3,13 +3,14 @@ package ru.stepup.demo;
 public class MainApp{
 
     public static void main(String[] args) {
-        int a=5;
+
         int b=6;
+        int c=3;
         System.out.println(1);
         // TODO поменять имя на enum
         System.out.println(2);
-        System.out.println(a);
         System.out.println(b);
+        System.out.println(c);
 
     }
 }
