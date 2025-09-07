e8fe086b68f122e137877d8abc60ceba813d1b39
Егор <xefver@gmail.com>
2025-09-07 17:59:36 +0300

    initial commit

---
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -0,0 +1,14 @@
+package ru.stepup.demo;
+
+public class MainApp{
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        System.out.println(1);
+        // TODO поменять имя на enum
+        System.out.println(2);
+        System.out.println(3);
+
+    }
+}
